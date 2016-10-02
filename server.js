7dd096f58ddceea2eed6d91b87e122f964b6386e
asishag12 <asishag12@gmail.com>
2016-10-02 19:34:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Personal', function(req, res){
+   res.sendFile(path.join(__dirname,'ui','personal.html')); 
+});
+
+app.get('/Professional', function(req, res){
+   res.sendFile(path.join(__dirname,'ui','professional.html')); 
+});
+
+app.get('/Social', function(req, res){
+   res.sendFile(path.join(__dirname,'ui','social.html')); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

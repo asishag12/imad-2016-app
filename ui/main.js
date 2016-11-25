f87b75bafaa2e537ed81a55fff315b4874e1fd37
asishag12 <asishag12@gmail.com>
2016-11-26 00:54:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,31 +5,37 @@ var pro= document.getElementById('professional');
 var soc= document.getElementById('social');
 
 per.onclick = function(){
-    var request1 = new XMLHttpRequest();
+   //var request1 = new XMLHttpRequest();
     
     //capture
-        request1.onreadystatechange = function(){
-        if(request1.readyState === XMLHttpRequest.DONE)
-        {
-            if(request1.status === 200){
-                var pers = request1.response.Text;
-                var pr = document.getElementById('data');
-                pr.innerHTML = pers.toString();
-            }
-        }
-    };
+      //  request1.onreadystatechange = function(){
+       // if(request1.readyState === XMLHttpRequest.DONE)
+        //{
+          //  if(request1.status === 200){
+               // var pers = request1.response.Text;
+                //var pr = document.getElementById('data');
+                //pr.innerHTML = pers.toString();
+            //}
+        //}
+   // };
     // make a request
-    request1.open('GET', 'http:asishag12.imad.hasura-app.io/personal', true);
-    request1.send(null);
+    //request1.open('GET', 'http:asishag12.imad.hasura-app.io/personal', true);
+   // request1.send(null);
+    //
+    
+    var a = document.getElementById('a');
+    a.href= 'asishag12.imad.hasura-app.io/personal.html';
     
 };
 
 pro.onclick = function(){
-    
+     var a = document.getElementById('a');
+    a.href= 'asishag12.imad.hasura-app.io/professional.html';
     
 };
 
 soc.onclick = function(){
-    
+     var a = document.getElementById('a');
+    a.href= 'asishag12.imad.hasura-app.io/social.html';
     
 };
